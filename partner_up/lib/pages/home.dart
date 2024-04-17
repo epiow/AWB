b1ee341fe917f3cef95a594b9d45cb720ddf5a6e
AtlastDeepLearning <89887461+AtlastDeepLearning@users.noreply.github.com>
2024-04-17 22:57:02 +0800

    first commit ni vic

---
+++ partner_up/lib/pages/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'CONNECT4IMPACT',
+          style: TextStyle(
+              color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/angle-small-left',
+            height: 20,
+            width: 20,
+          ),
+          decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+    );
+  }
+}
